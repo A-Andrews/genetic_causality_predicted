0dda03f0412b1d0e19213040ed2be2b750435ed0
Austin Andrews <nrw600@cluster1.hpc.in.bmrc.ox.ac.uk>
2025-06-27 10:16:43 +0100

    init commit

---
+++ data_consolidation/data_loading.py
@@ -0,0 +1,28 @@
+import os
+import settings
+
+def load_baselineLD_annotations(file_path):
+    """
+    Load baseline LD annotations from a predefined file path.
+    
+    Returns:
+    """
+    return
+
+def load_bim_file(file_path):
+    """
+    """
+    return
+
+def load_traitgym_data(split):
+    """
+    Load TraitGym dataset splits
+
+    Returns:
+    """
+    return
+
+def merge_varient_features(traitgym_df, annotation_df):
+    """
+    """
+    return
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+
+BASELINELD_PATH = os.path("well", "palamara", "projects", "S-LDSC_reference_files", "GRCh38", "baselineLD_v2.2")
+PLINK_PATH = os.path("well", "palamara", "projects", "S-LDSC_reference_files", "GRCh38", "plink_files")
+TRAITGYM_PATH = os.path("well", "palamara", "users", "nrw600", "contribution_prediction", "TraitGym", "results", "dataset")
